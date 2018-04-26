2bd9c53a4c0037d87263a642311fe4d9cdbc4d1a
Duy Dương <phanduyduong97@gmail.com>
2018-04-26 21:47:20 +0700

    [dương] tạo mô hình 3 lớp

---
+++ QuanLyQuanCafe/QuanLyQuanCafe/DAL/KetNoi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyQuanCafe.DAL
+{
+    public class KetNoi
+    {
+        private SqlConnection conn;
+        public KetNoi()
+        {
+            conn = new SqlConnection(@"Data Source=ADMIN-PC\SQLEXPRESS;Initial Catalog=QuanLyQuanCafe;Integrated Security=True");
+        }
+        public DataSet GetData(string strSql)
+        {
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(strSql, conn);
+            conn.Open();
+            da.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+        public DataSet GetData(string NameProc, SqlParameter[] para)
+        {
+
+            SqlCommand cmd = new SqlCommand(NameProc, conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (para != null)
+                cmd.Parameters.AddRange(para);
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet();
+            conn.Open();
+            da.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+        public int ExcuteSQL(string strSQL)
+        {
+            SqlCommand cmd = new SqlCommand(strSQL, conn);
+            conn.Open();
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+            return count;
+        }
+        public int ExcuteSQL(string NameProc, SqlParameter[] para)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = NameProc;
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (para != null)
+            {
+                cmd.Parameters.AddRange(para);
+            }
+            cmd.Connection = conn;
+            conn.Open();
+            int count = cmd.ExecuteNonQuery();
+            conn.Close();
+            return count;
+        }
+    }
+}
